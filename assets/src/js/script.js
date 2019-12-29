b252fa06533ac1343911cc3fd3a28a34bb6d1c52
Kevin Marquegnies <kevin.marquegnies@gmail.com>
2019-12-29 23:08:36 +0100

    finish hobbies section with animation when mouse enter and leave pictures

--- assets/src/js/script.js
+++ assets/src/js/script.js
@@ -1,3 +1,6 @@
+
+/*--FIXE LE MENU EN HAUT DE LA PAGE--*/
+
 //Récupère le menu dans une variable
 var menu = document.getElementsByTagName("nav")[0];
 
@@ -7,7 +10,7 @@ var fixe = menu.offsetTop;
 /*Si la position offset de la page est supérieure ou 
 égale à la position offset du menu alors le menu 
 obtient la classe fixed qui le rend fixe et 
-le faire suivre le srcoll */
+le faire suivre le srcoll*/
 
 window.addEventListener("scroll", function () {
   if (window.pageYOffset >= fixe) {
@@ -16,3 +19,24 @@ window.addEventListener("scroll", function () {
     menu.classList.remove("fixed");
   }
 });
+
+
+/*--FAIS APPARAITRE LE TEXTE DES IMAGES LOISIR--*/
+function textAppear(number) {
+  var text = document.getElementsByClassName("picture_text")[number];
+  var pictureBloc = document.getElementsByClassName("picture_animation")[number];
+  var picture = document.getElementsByClassName("hobby_picture")[number];
+  
+  picture.style.opacity = "0.3";
+  text.style.visibility = "visible";
+  pictureBloc.addEventListener("mouseout", function(){
+    text.style.visibility = "hidden";
+    picture.style.opacity = "1";
+  });
+}
+
+
+/*--FAIS DISPARAITRE LE TEXTE DES IMAGES LOISIR--*/
+/*function textDisappaer(number) {
+  var
+}*/
